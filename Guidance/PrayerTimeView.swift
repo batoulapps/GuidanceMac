6c8fa54896788ff0d8cd5504b6b992a95cf01917
Ameir Al-Zoubi <ameir.alzoubi@gmail.com>
2020-02-22 20:31:39 -0500

    fix prayer time alignment

--- Guidance/PrayerTimeView.swift
+++ Guidance/PrayerTimeView.swift
@@ -20,6 +20,10 @@ class PrayerTimeView: NSView {
     private static let rowWidth: CGFloat = 200
     private static let rowHeight: CGFloat = 21
     private static let rowPadding: CGFloat = 21
+    private static let baseAttributes: [NSAttributedString.Key : Any] = [
+        .font: NSFont.menuFont(ofSize: 14),
+        .foregroundColor: NSColor.textColor
+    ]
 
     let formatter: DateFormatter = {
         let d = DateFormatter()
@@ -31,34 +35,50 @@ class PrayerTimeView: NSView {
     override func draw(_ dirtyRect: NSRect) {
         super.draw(dirtyRect)
         
-        let attributes: [NSAttributedString.Key : Any] = [
-            .font: NSFont.menuFont(ofSize: 14),
-            .foregroundColor: NSColor.textColor
-        ]
+        let nameAttr = nameAttributes()
+        let timeAttr = timeAttributes()
 
         for (index, prayer) in Prayer.allCases.enumerated() {
-            prayer.name.draw(in: nameRect(row: index), withAttributes: attributes)
-            if let time = prayerTimes?.time(for: prayer) {
-                (formatter.string(from: time) as NSString).draw(in: timeRect(row: index), withAttributes: attributes)
-            } else {
-
-            }
+            let time: NSString = {
+                guard let prayerTime = prayerTimes?.time(for: prayer) else {
+                    return "-:-"
+                }
+                return formatter.string(from: prayerTime) as NSString
+            }()
+            prayer.name.draw(in: nameRect(row: index), withAttributes: nameAttr)
+            time.draw(in: timeRect(row: index), withAttributes: timeAttr)
         }
     }
 
+    private func nameAttributes() -> [NSAttributedString.Key : Any] {
+        var attributes = PrayerTimeView.baseAttributes
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = .left
+        attributes[.paragraphStyle] = paragraphStyle
+        return attributes;
+    }
+
+    private func timeAttributes() -> [NSAttributedString.Key : Any] {
+        var attributes = PrayerTimeView.baseAttributes
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = .right
+        attributes[.paragraphStyle] = paragraphStyle
+        return attributes;
+    }
+
     private func nameRect(row: Int) -> NSRect {
-        let nameWidth = (PrayerTimeView.rowWidth - (PrayerTimeView.rowPadding * 2)) / 2
-        let nameHeight = PrayerTimeView.rowHeight
+        let width = (PrayerTimeView.rowWidth - (PrayerTimeView.rowPadding * 2)) / 2
+        let height = PrayerTimeView.rowHeight
         let xPos = PrayerTimeView.rowPadding
-        let yPos = PrayerTimeView.menuItemFrame.height - (nameHeight * CGFloat(row + 1))
-        return NSRect(x: xPos, y: yPos, width: nameWidth, height: nameHeight)
+        let yPos = PrayerTimeView.menuItemFrame.height - (height * CGFloat(row + 1))
+        return NSRect(x: xPos, y: yPos, width: width, height: height)
     }
 
     private func timeRect(row: Int) -> NSRect {
-        let nameWidth = (PrayerTimeView.rowWidth - (PrayerTimeView.rowPadding * 2)) / 2
-        let nameHeight = PrayerTimeView.rowHeight
-        let xPos = PrayerTimeView.rowPadding + (PrayerTimeView.rowWidth / 2)
-        let yPos = PrayerTimeView.menuItemFrame.height - (nameHeight * CGFloat(row + 1))
-        return NSRect(x: xPos, y: yPos, width: nameWidth, height: nameHeight)
+        let width = (PrayerTimeView.rowWidth - (PrayerTimeView.rowPadding * 2)) / 2
+        let height = PrayerTimeView.rowHeight
+        let xPos = PrayerTimeView.rowWidth / 2
+        let yPos = PrayerTimeView.menuItemFrame.height - (height * CGFloat(row + 1))
+        return NSRect(x: xPos, y: yPos, width: width, height: height)
     }
 }
